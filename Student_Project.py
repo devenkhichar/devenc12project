baf0404da8889212aee9c2b2b4db43d65a9f38cc
Deven khichar <95070492+devenkhichar@users.noreply.github.com>
2022-04-05 18:21:52 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,24 @@
+def inputs():
+    Name = input("Enter your name :")
+    Surname = input("Enter your surname :")
+    Mob_No = int(input("Enter the mobile number :"))
+    a_string = str(Mob_No)
+    length=len(a_string)
+    
+    if length == 10:
+        print("a valid number")
+    else:
+        print("not a valid no")
+ 
+    Birth_Date =(input("Enter your birth date in dd-mm-yyyy format:"))
+    Age = float(input("Enter your age with months after decimal point :"))#Convert the input such that it can accept float value)
+    print("Form is submitted")
+    print("Not a valid number. Please enter a valid number")
+        
+    #print(("\nThe details of the person are :\n","\nThe name is :\t", Name), ("\nThe surname is :\t",Surname) , ("\nThe mobile number is :\t",Mob_No) , ("\nThe birth date is :\t",Birth_Date ), ("\nThe age is :\t",Age))
+    #mam what is the problem in above line paste the answer in comment of my project (left else is right)
+
+
+inputs()       
+
+
